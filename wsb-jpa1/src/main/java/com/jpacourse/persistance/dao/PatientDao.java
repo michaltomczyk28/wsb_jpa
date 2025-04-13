ce731379dd4f0207c7fac8dfa0ec7d09293f3b04
Szymon Stryjewski <szymon.stryjewski48@gmail.com>
2025-04-13 11:26:49 +0200

    findByPatient firstName lastName added test done

--- wsb-jpa1/src/main/java/com/jpacourse/persistance/dao/PatientDao.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistance/dao/PatientDao.java
@@ -3,10 +3,14 @@ package com.jpacourse.persistance.dao;
 import com.jpacourse.persistance.entity.PatientEntity;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface PatientDao extends Dao<PatientEntity, Long> {
 
     void addVisitToPatient(Long patientId, Long doctorId, LocalDateTime visitDate, String visitDescription);
 
+    public List<PatientEntity> findByPatientFirstName(String patientName);
+    public List<PatientEntity> findByVisitDate(LocalDateTime visitDate);
+    public List<PatientEntity> findByPatientLastName(String patientLastName);
 
 }
