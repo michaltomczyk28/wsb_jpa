d22ec40cd9fe519f8bf3f0557f3df3eae9e1b24e
Michal Tomczyk <michaltomczyk28@gmail.com>
2025-04-22 21:14:47 +0200

    implement findAllThatUsedToBeMarried() in the PatientDao

--- wsb-jpa1/src/main/java/com/jpacourse/persistance/dao/PatientDao.java
+++ wsb-jpa1/src/main/java/com/jpacourse/persistance/dao/PatientDao.java
@@ -8,7 +8,8 @@ public interface PatientDao extends Dao<PatientEntity, Long> {
 
     void addVisitToPatient(Long patientId, Long doctorId, LocalDateTime visitDate, String visitDescription);
 
-    public List<PatientEntity> findByPatientFirstName(String patientName);
-    public List<PatientEntity> findByPatientLastName(String patientLastName);
-    public List<PatientEntity> findPatientsWithMoreThanXVisits(int visitCount);
+    List<PatientEntity> findByPatientFirstName(String patientName);
+    List<PatientEntity> findByPatientLastName(String patientLastName);
+    List<PatientEntity> findPatientsWithMoreThanXVisits(int visitCount);
+    List<PatientEntity> findAllThatUsedToBeMarried();
 }
